05d040e2431a3dc3c722a3450cee903417a4ee75
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2018-12-04 20:38:46 +0700

    Created postgresQL db

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import { ApolloServer, gql } from 'apollo-server';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import models from './models';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-)
+models.sequelize.sync().then(() => {
+  server.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  )
+})
---
+++ models/index.js
@@ -0,0 +1,24 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('slack', 'dianka', 'dianka', {
+  dialect: 'postgres'
+});
+
+const models = {
+  User: sequelize.import('./user'),
+  Team: sequelize.import('./team'),
+  Message: sequelize.import('./message'),
+  Channel: sequelize.import('./channel'),
+};
+
+
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default models;
