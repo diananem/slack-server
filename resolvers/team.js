7dd41d301b616bb4aea8add75c836d754720ff4f
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2018-12-14 10:28:09 +0700

    Auth permissions

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,20 +1,23 @@
 import formatErrors from "../formatErrors";
+import requiresAuth from "../permissions";
 
 export default {
   Mutation: {
-    createTeam: async (parent, args, { models, user }) => {
-      try {
-        await models.Team.create({ ...args, owner: user.id });
-        return {
-          success: true
-        };
-      } catch (err) {
-        // console.error(err);
-        return {
-          success: false,
-          errors: formatErrors(err, models)
-        };
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Team.create({ ...args, owner: user.id });
+          return {
+            success: true
+          };
+        } catch (err) {
+          // console.error(err);
+          return {
+            success: false,
+            errors: formatErrors(err, models)
+          };
+        }
       }
-    }
+    )
   }
 };
