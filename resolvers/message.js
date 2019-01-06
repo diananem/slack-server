04bae3a40286cc12009833b9080acf50c8a7c155
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2019-01-06 19:20:08 +0700

    Added messages query

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,13 +1,30 @@
+import requiresAuth from "../permissions";
+
 export default {
+  Message: {
+    user: ({ user_id }, args, { models }) =>
+      models.User.findOne({ where: { id: user_id } })
+  },
+  Query: {
+    messages: requiresAuth.createResolver(
+      async (parent, { channel_id }, { models, user }) =>
+        models.Message.findAll(
+          { order: [["created_at", "ASC"]], where: { channel_id } },
+          { raw: true }
+        )
+    )
+  },
   Mutation: {
-    createMessage: async (parents, args, { models, user }) => {
-      try {
-        await models.Message.create({ ...args, user_id: user.id });
-        return true;
-      } catch (err) {
-        console.error(err)
-        return false;
+    createMessage: requiresAuth.createResolver(
+      async (parents, args, { models, user }) => {
+        try {
+          await models.Message.create({ ...args, user_id: user.id });
+          return true;
+        } catch (err) {
+          console.error(err);
+          return false;
+        }
       }
-    }
+    )
   }
 };
--- schema/message.js
+++ schema/message.js
@@ -5,7 +5,12 @@ export default gql`
     id: Int!
     text: String!
     user: User!
-    channnel: Channel!
+    channel: Channel!
+    created_at: String!
+  }
+
+  type Query {
+    messages(channel_id: Int!): [Message!]!
   }
   type Mutation {
     createMessage(channel_id: Int!, text: String!): Boolean!
